428fd3449b74a39af7d80370a617f33a929d5930
Billy Fletcher <billy@billyfletcher.com>
2024-08-30 22:53:31 +0100

    Added a simple delay node and it works properly and everything, non-instant nodes do have to manually track based on pathway but it does work

---
+++ Plugins/Cadence/Source/Cadence/Private/CadenceDelayNodes.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CadenceDelayNodes.h"
+
+#include "CadenceContext.h"
+#include "CadenceGraphNodePin.h"
+#include "CadencePinConstants.h"
+#include "CadenceVariable.h"
+
+void UCadenceDelayNode::CreateInputPins()
+{
+	Super::CreateInputPins();
+	AddInputVariablePin(FCadencePinConstants::Pin_Duration, UCadenceVariableFloat::StaticClass());
+}
+
+bool UCadenceDelayNode::Execute(UCadenceContext* InContext)
+{
+	UCadenceGraphNodePin* InputPin = GetInputPin(FCadencePinConstants::Pin_Duration);
+	UCadenceVariableFloat* InputFloatVariable = InputPin->GetVariable<UCadenceVariableFloat>();
+
+	float Duration = InputFloatVariable->GetValue();
+	float TimeElapsed = 0.0f;
+	
+	if(TimeElapsedMap.Contains(InContext->Pathway))
+		TimeElapsed = TimeElapsedMap[InContext->Pathway];
+	else
+		TimeElapsedMap.Add(InContext->Pathway, 0.0f);
+
+	TimeElapsed += InContext->DeltaSeconds;
+	if(TimeElapsed > Duration)
+	{
+		TimeElapsedMap.Remove(InContext->Pathway);
+		return true;
+	}
+	else
+	{
+		TimeElapsedMap[InContext->Pathway] = TimeElapsed;
+		return false;
+	}
+}
---
+++ Plugins/Cadence/Source/Cadence/Public/CadenceDelayNodes.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CadenceGraphNode.h"
+#include "UObject/Object.h"
+#include "CadenceDelayNodes.generated.h"
+
+class UCadenceGraphRunnerPathway;
+
+namespace FCadenceDelayConstants
+{
+	static const FText NodeCategory = FText::FromString(TEXT("Delays"));
+	static constexpr FLinearColor NodeTitleColor = FLinearColor(0.3f, 0.6f, 0.9f);
+}
+
+/**
+ * 
+ */
+UCLASS()
+class CADENCE_API UCadenceDelayNode : public UCadenceGraphNode
+{
+	GENERATED_BODY()
+
+public:
+	virtual void CreateInputPins() override;
+	virtual bool Execute(UCadenceContext* InContext) override;
+	
+	virtual FText GetNodeMenuName() const override { return FText::FromString(TEXT("Delay")); }
+	virtual FText GetNodeCategory() const override { return FCadenceDelayConstants::NodeCategory; }
+	virtual FLinearColor GetNodeTitleColor() const override { return FCadenceDelayConstants::NodeTitleColor; }
+
+private:
+	TMap<UCadenceGraphRunnerPathway*, float> TimeElapsedMap;
+};
