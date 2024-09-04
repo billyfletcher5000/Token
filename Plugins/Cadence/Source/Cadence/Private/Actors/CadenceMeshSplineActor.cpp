27ba75379d904367b0c875a40c8482a28365d455
Billy Fletcher <billy@billyfletcher.com>
2024-09-04 21:36:48 +0100

    Added mesh spline that can also render to the stencil buffer setup, should be good for doing lines

---
+++ Plugins/Cadence/Source/Cadence/Private/Actors/CadenceMeshSplineActor.cpp
@@ -0,0 +1,62 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/CadenceMeshSplineActor.h"
+
+#include "Components/SplineComponent.h"
+#include "Components/SplineMeshComponent.h"
+
+
+// Sets default values
+ACadenceMeshSplineActor::ACadenceMeshSplineActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	SplineComponent = this->CreateDefaultSubobject<USplineComponent>(TEXT("Spline"));
+	SetRootComponent(SplineComponent);
+}
+
+void ACadenceMeshSplineActor::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	int32 NumPoints = SplineComponent->GetNumberOfSplinePoints();
+	
+	for(int32 CurrentIndex = 0; CurrentIndex < NumPoints - 1; ++CurrentIndex)
+	{		
+		USplineMeshComponent* SplineMeshComponent = NewObject<USplineMeshComponent>(this, USplineMeshComponent::StaticClass());
+		SplineMeshComponent->RegisterComponentWithWorld(GetWorld());
+		SplineMeshComponent->CreationMethod = EComponentCreationMethod::UserConstructionScript;
+		SplineMeshComponent->SetMobility(EComponentMobility::Movable);  
+		SplineMeshComponent->AttachToComponent(SplineComponent, FAttachmentTransformRules::KeepRelativeTransform);
+
+		SplineMeshComponent->SetVisibleInSceneCaptureOnly(bVisibleInSceneCaptureOnly);
+		SplineMeshComponent->SetRenderCustomDepth(bRenderCustomDepth);
+		SplineMeshComponent->SetCustomDepthStencilValue(CustomDepthStencilValue);
+		SplineMeshComponent->SetCustomDepthStencilWriteMask(CustomDepthStencilWriteMask);
+		
+		if(CurrentIndex == 0 && bHasSpecificStartMesh)
+		{
+			SplineMeshComponent->SetStaticMesh(SplineMeshStart);
+		}
+		else if(CurrentIndex == NumPoints - 2 && bHasSpecificEndMesh)
+		{
+			SplineMeshComponent->SetStaticMesh(SplineMeshEnd);
+		}
+		else
+		{
+			SplineMeshComponent->SetStaticMesh(SplineMeshMiddle);
+		}
+
+		FVector StartPosition = SplineComponent->GetLocationAtSplinePoint(CurrentIndex, ESplineCoordinateSpace::Local);
+		FVector StartTangent = SplineComponent->GetTangentAtSplinePoint(CurrentIndex, ESplineCoordinateSpace::Local);
+		FVector EndPosition = SplineComponent->GetLocationAtSplinePoint(CurrentIndex + 1, ESplineCoordinateSpace::Local);
+		FVector EndTangent = SplineComponent->GetTangentAtSplinePoint(CurrentIndex + 1, ESplineCoordinateSpace::Local);
+
+		SplineMeshComponent->SetStartScale(SplineMeshScale);
+		SplineMeshComponent->SetEndScale(SplineMeshScale);
+		
+		SplineMeshComponent->SetForwardAxis(ForwardAxis);
+		SplineMeshComponent->SetStartAndEnd(StartPosition, StartTangent, EndPosition, EndTangent);		
+	}	
+}
---
+++ Plugins/Cadence/Source/Cadence/Public/Actors/CadenceMeshSplineActor.h
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SplineMeshComponent.h"
+
+#include "CadenceMeshSplineActor.generated.h"
+
+class USplineComponent;
+
+UCLASS()
+class CADENCE_API ACadenceMeshSplineActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ACadenceMeshSplineActor();
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USplineComponent> SplineComponent;
+
+	UPROPERTY(EditAnywhere, Category="Spline Mesh")
+	bool bHasSpecificStartMesh = false;
+	
+	UPROPERTY(EditAnywhere, Category="Spline Mesh")
+	bool bHasSpecificEndMesh = false;
+	
+	UPROPERTY(EditAnywhere, meta=(EditCondition="bHasSpecificStartMesh", EditConditionHides), Category="Spline Mesh")
+	TObjectPtr<UStaticMesh> SplineMeshStart;
+
+	UPROPERTY(EditAnywhere, Category="Spline Mesh")
+	TObjectPtr<UStaticMesh> SplineMeshMiddle;
+
+	UPROPERTY(EditAnywhere, meta=(EditCondition="bHasSpecificEndMesh", EditConditionHides), Category="Spline Mesh")
+	TObjectPtr<UStaticMesh> SplineMeshEnd;
+
+	UPROPERTY(EditAnywhere, Category="Spline Mesh")
+	FVector2D SplineMeshScale = FVector2D::One();
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Spline Mesh")
+	TEnumAsByte<ESplineMeshAxis::Type> ForwardAxis = ESplineMeshAxis::X;
+
+	UPROPERTY(EditAnywhere, Category="Rendering")
+	bool bVisibleInSceneCaptureOnly = true;
+
+	UPROPERTY(EditAnywhere, Category="Rendering")
+	bool bRenderCustomDepth = true;
+
+	UPROPERTY(EditAnywhere, Category="Rendering")
+	int32 CustomDepthStencilValue = 1;
+
+	UPROPERTY(EditAnywhere, Category="Rendering")
+	ERendererStencilMask CustomDepthStencilWriteMask = ERendererStencilMask::ERSM_Default;
+};
