26ade9401656c55ea12bc0c92b2246f271c9d7b7
Billy Fletcher <billy@billyfletcher.com>
2025-06-28 21:30:48 +0100

    Removed dependency on terrible array helper plugin by temporarily adding in equivalent function in a BPFL

---
+++ Source/Token/Private/TokenArrayHelperBPFL.cpp
@@ -0,0 +1,12 @@
+﻿#include "TokenArrayHelperBPFL.h"
+
+void UTokenArrayHelperBPFL::SortByPredicate(TArray<AActor*>& InOutArray, const FSortByPredicateActor& InPredicateActor)
+{
+	if (InPredicateActor.IsBound())
+	{
+		Algo::Sort(InOutArray, [&InPredicateActor](AActor* A1, AActor* A2) -> bool
+		{
+			return InPredicateActor.Execute(A1, A2);
+		});
+	}
+}
---
+++ Source/Token/Public/TokenArrayHelperBPFL.h
@@ -0,0 +1,22 @@
+﻿
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "TokenArrayHelperBPFL.generated.h"
+
+DECLARE_DYNAMIC_DELEGATE_RetVal_TwoParams(bool, FSortByPredicateActor, class AActor*, InActorA, class AActor*, InActorB);
+
+/**
+ * 
+ */
+UCLASS()
+class TOKEN_API UTokenArrayHelperBPFL : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Array")
+	static void SortByPredicate(UPARAM(ref) TArray<AActor*>& InOutArray, const FSortByPredicateActor& InPredicateActor);
+};
